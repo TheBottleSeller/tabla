a5baf2a9abbc07694796988f83c4878a826fe2f1
Neil Batlivala <neil@goforward.com>
2017-08-19 16:32:56 -0700

    Fix up validation

--- validate_audio_files.py
+++ validate_audio_files.py
@@ -8,7 +8,7 @@ parser.add_argument('--study', type=str, help='ED or PNA')
 args = parser.parse_args()
 
 study = args.study
-audio_file_path = args.audio_file_path
+audio_file_path = args.audio_file_path.rstrip('/')
 if not audio_file_path:
     print('audio file path is required')
     sys.exit(-1)
@@ -27,43 +27,52 @@ def represents_int(s):
     except ValueError:
         return False
 
-def is_valid_audio_filename(filename):
+def is_valid_audio_filename(type, filename):
     if not filename.endswith('.wav'):
         return False
     filename = filename[:-4]
     parts = filename.split('_')
 
-    if not len(parts) == 2:
+    if not len(parts) == 3:
+        print 'here1'
+        return False
+        
+    if not parts[0] == type:
+        print type
+        return False
+    
+    if (parts[1] != 'LLL' and
+        parts[1] != 'LML' and
+        parts[1] != 'LUL' and
+        parts[1] != 'RLL' and
+        parts[1] != 'RML' and
+        parts[1] != 'RUL'):
         return False
 
-    if not represents_int(parts[1]):
-        return False
-
-    if (parts[0] != 'LLL' and
-        parts[0] != 'LML' and
-        parts[0] != 'LUL' and
-        parts[0] != 'RLL' and
-        parts[0] != 'RML' and
-        parts[0] != 'RUL'):
+    if not represents_int(parts[2]):
         return False
 
     return True
 
 # BS, PS, or TF
-def verify_type_dir(type_dir):
+def verify_type_dir(patient, type):
+    type_dir = "%s/%s/%s" % (audio_file_path, patient, type)
+    if not os.path.isdir(type_dir):
+        return
+    
     audio_files = [file for file in os.listdir(type_dir) if file != 'Icon\r']
 
     for file in audio_files:
-        if not is_valid_audio_filename(file):
+        if not is_valid_audio_filename(type, file):
             raise Exception('Bad audio file name: %s' % file)
 
     def verify_no_missing_trials(location):
-        recordings = [f for f in audio_files if f.startswith(location)]
+        recordings = [f for f in audio_files if f.startswith("%s_%s" % (type, location))]
         if len(recordings) == 0:
             raise Exception('Missing %s trials for %s' % (location, type_dir))
 
         for i in range(len(recordings)):
-            expected_trial = "%s_%d.wav" % (location, i + 1)
+            expected_trial = "%s_%s_%d.wav" % (type, location, i + 1)
             if expected_trial not in audio_files:
                 raise Exception('Missing trial %s for %s' % (expected_trial, type_dir))
 
@@ -74,27 +83,19 @@ def verify_type_dir(type_dir):
     verify_no_missing_trials('RML')
     verify_no_missing_trials('RUL')
 
-def verify_patient_dir(patient_dir, last_seen_id):
+# patient PNA001, ED001
+def verify_patient_dir(patient, last_seen_id):
     # Check not missing a patient data dir
-    id = int(patient_dir[len(study):])
+    id = int(patient[len(study):])
     if not id == last_seen_id + 1:
         print('Missing patient data %d' % (last_seen_id + 1))
         sys.exit(-1)
 
-    # Check BS data
-    bs_path = "%s%s/BS" % (audio_file_path, patient_dir)
-    if os.path.isdir(bs_path):
-        verify_type_dir(bs_path)
+    verify_type_dir(patient, "BS")
+    verify_type_dir(patient, "PS")
+    verify_type_dir(patient, "TF")
 
-    # Check PS data
-    ps_path = "%s%s/PS" % (audio_file_path, patient_dir)
-    if os.path.isdir(ps_path):
-        verify_type_dir(ps_path)
-
-    # Check TF data
-    tf_path = "%s%s/TF" % (audio_file_path, patient_dir)
-    if os.path.isdir(tf_path):
-        verify_type_dir(tf_path)
+print "Validating %s study" % study
 
 last_seen_id = 0
 for patient_dir in patient_dirs:
@@ -104,3 +105,5 @@ for patient_dir in patient_dirs:
         print('Failed validation patient %s' % patient_dir)
         print(error)
     last_seen_id = last_seen_id + 1
+    
+print "Done"
