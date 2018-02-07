b124527f7ee2a286998d7dd8fb458c77bf773f30
Neil Batlivala <neil@goforward.com>
2018-02-06 19:31:47 -0800

    Trim audio

--- cleaning/isolate_chirp.py
+++ cleaning/isolate_chirp.py
@@ -4,113 +4,86 @@ import numpy as np
 from scipy.signal import get_window
 import math
 import matplotlib.pyplot as plt
-
+import argparse
+from pydub import AudioSegment
 sys.path.insert(0, '../tools')
 import stft
 import utilFunctions as UF
 
-eps = np.finfo(float).eps
+window = 'blackman' # analysis window type (choice of rectangular, triangular, hanning, hamming, blackman, blackmanharris)
+M = 961 # (integer): analysis window size (odd integer value)
+N = 2048 # (integer): fft size (power of two, bigger or equal than than M)
+H = 128 # (integer): hop size for the STFT computation
 
-def dB2energydB(mdB):
-    m = 10 ** (mdB / 20.)
-    energy_ = m ** 2.
+parser = argparse.ArgumentParser(description='Clean audio file')
+parser.add_argument('--in', type=str, help='input file')
+parser.add_argument('--out', type=str, help='output file')
+input_path = args.in
+output_path = args.out
 
-    #m = 10 * np.log10(m.sum())
-    energy_ = 10 * np.log10(np.sum(energy_))
-
-    return energy_
-
-def findChirpEnd(inputFile, window, M, N, H):
-    """
-    Inputs:
-            inputFile (string): input sound file (monophonic with sampling rate of 44100)
-            window (string): analysis window type (choice of rectangular, triangular, hanning, hamming,
-                blackman, blackmanharris)
-            M (integer): analysis window size (odd integer value)
-            N (integer): fft size (power of two, bigger or equal than than M)
-            H (integer): hop size for the STFT computation
-    Output:
-            The function should return a numpy array with two columns, where the first column is the ODF
-            computed on the low frequency band and the second column is the ODF computed on the high
-            frequency band.
-            ODF[:,0]: ODF computed in band 0 < f < 3000 Hz
-            ODF[:,1]: ODF computed in band 3000 < f < 10000 Hz
-    """
+def findChirpEnd(inputFile):
+    def dB2energydB(mdB):
+        m = 10 ** (mdB / 20.)
+        energy_ = m ** 2.
+        #m = 10 * np.log10(m.sum())
+        energy_ = 10 * np.log10(np.sum(energy_))
+        return energy_
 
     (fs, x) = UF.wavread(inputFile)
-
-    w = get_window(window,M)
-
+    w = get_window(window, M)
     xmX, xpX = stft.stftAnal(x, w, N, H)
-
-    #Get number of frames (time slices)
-    numFrames = int(xmX[:,0].size)
-
-    #Creating array of bin frequencies (positive side only)
-    binFreq = np.arange(N/2+1)*float(fs)/N
-
-    #Locate the first bin frequency index and last bin frequency index in both desired ranges
-
-    lowBandIdx0 = np.where(binFreq > 40)[0][0]
-
-    lowBandIdx3000 = np.where(binFreq < 3000)[0][-1]
+    numFrames = int(xmX[:,0].size) #Get number of frames (time slices)
+    binFreq = np.arange(N/2+1)*float(fs)/N #Creating array of bin frequencies (positive side only)
 
     highBandIdx3000 = np.where(binFreq > 1000)[0][0]
-
     highBandIdx10000 = np.where(binFreq < 1002)[0][-1]
 
     # calculate energy per band
-    engEnv = np.zeros([numFrames, 2])
+    engEnv = np.zeros([numFrames])
     for idx_frame in range(numFrames):
-        engEnv[idx_frame, 0] = dB2energydB(xmX[idx_frame,lowBandIdx0:lowBandIdx3000+1])
-        engEnv[idx_frame, 1] = dB2energydB(xmX[idx_frame, highBandIdx3000:highBandIdx10000+1])
+        engEnv[idx_frame] = dB2energydB(xmX[idx_frame, highBandIdx3000:highBandIdx10000+1])
 
-    odf = np.zeros(engEnv.shape)
-    for i in range(0, engEnv.shape[0]):
-        if i == 0:
-            odf[0] = [0, 0]
-        else:
-            odf[i][0] = engEnv[i][0] - engEnv[i-1][0]
-            odf[i][1] = engEnv[i][1] - engEnv[i-1][1]
+    # plt.figure(1, figsize=(9.5, 6))
+    #
+    # plt.subplot(211)
+    # numFrames = int(xmX[:,0].size)
+    # frmTime = H*np.arange(numFrames)/float(fs)
+    # binFreq = np.arange(N/2+1)*float(fs)/N
+    # plt.pcolormesh(frmTime, binFreq, np.transpose(xmX))
+    # plt.title('mX (piano.wav), M=1001, N=1024, H=256')
+    # plt.autoscale(tight=True)
+    #
+    # plt.subplot(212)
+    # numFrames = int(xmX[:,0].size)
+    # frmTime = H*np.arange(numFrames)/float(fs)
+    # binFreq = np.arange(N/2+1)*float(fs)/N
+    # #plt.pcolormesh(frmTime, binFreq, np.diff(np.transpose(xpX),axis=0))
+    # #plt.plot(odf[:,0])
+    # plt.plot(abs(odf[:,1]))
+    # plt.title('ODF adsfsf')
+    # plt.autoscale(tight=True)
+    #
+    # plt.tight_layout()
+    # plt.savefig('spectrogram.png')
+    # plt.show()
 
-    plt.figure(1, figsize=(9.5, 6))
-
-    plt.subplot(211)
-    numFrames = int(xmX[:,0].size)
-    frmTime = H*np.arange(numFrames)/float(fs)
-    binFreq = np.arange(N/2+1)*float(fs)/N
-    plt.pcolormesh(frmTime, binFreq, np.transpose(xmX))
-    plt.title('mX (piano.wav), M=1001, N=1024, H=256')
-    plt.autoscale(tight=True)
-
-    plt.subplot(212)
-    numFrames = int(xmX[:,0].size)
-    frmTime = H*np.arange(numFrames)/float(fs)
-    binFreq = np.arange(N/2+1)*float(fs)/N
-    #plt.pcolormesh(frmTime, binFreq, np.diff(np.transpose(xpX),axis=0))
-    #plt.plot(odf[:,0])
-    plt.plot(abs(odf[:,1]))
-    plt.title('ODF adsfsf')
-    plt.autoscale(tight=True)
-
-    plt.tight_layout()
-    plt.savefig('spectrogram.png')
-    plt.show()
-
-    #maxIndex = np.argmax(abs(odf[:,1]))
-    maxIndex = np.argmax(engEnv[:,1])
-    timePercent = maxIndex * 1.0 / odf[:,1].size
+    maxIndex = np.argmax(engEnv)
+    timePercent = maxIndex * 1.0 / engEnv.size
     audioLength = x.size / fs
-    print timePercent
     end_of_chirp = audioLength * timePercent
-    print audioLength
     return end_of_chirp
-
-end_of_chirp = findChirpEnd('./chirp.wav', 'blackman', 961, 2048, 128)
-print end_of_chirp
 
 # The chirp lasts exactly 14 seconds
 # we need to make sure we capture all of it
 # If procedure missed some parts of the beginning of the chirp
 # that is ok, but we need to account for that
+end_of_chirp = findChirpEnd(input_path, 'blackman', 961, 2048, 128)
 start_of_chirp = max(0, end_of_chirp - 14.0)
+
+# Round to milliseconds
+start_ms = format(start_of_chirp, '.2f')
+end_ms = format(end_of_chirp, '.2f')
+
+chirp = AudioSegment.from_wav(input_path)
+isolated_chirp = chirp[start_ms:end_ms]
+faded_chirp = isolated_chirp.fade_in(2000).fade_out(3000)
