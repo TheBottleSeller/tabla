cafda67454cb862125a9882eda4e8239c423a9f3
adamjrao <adam.j.rao@gmail.com>
2018-02-16 23:52:16 -0800

    Fix up running of features

--- features/mfcc.py
+++ features/mfcc.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 import essentia.standard as ess
@@ -7,6 +8,8 @@ N = 1024
 H = 512
 fs = 4000
 
+# Calculate the mean mfcc from coefficients 1-12 for each PS recording
+# Average all the mfcc results across the ps recordings
 def get_features(patient_dir):
 	ps_recordings = [file for file in os.listdir("%s/PS" % patient_dir) if file.endswith(".wav")]
 	ps_features = []
@@ -15,10 +18,8 @@ def get_features(patient_dir):
 		hs, features = _get_features("%s/PS/%s" % (patient_dir, recording))
 		headers = hs
 		ps_features.append(features)
-	print ps_features
-	average_ps_features = np.mean(ps_features, axis=1)
-	print average_ps_features
-	return headers, average_ps_features
+	average_ps_features = np.mean(ps_features, axis=0)
+	return headers, average_ps_features.tolist()
 
 def _get_features(audio_path):
 	spectrum = ess.Spectrum(size=N)
@@ -40,26 +41,26 @@ def _get_features(audio_path):
 		headers.append('mean mfcc %d' % i)
 		features.append(np.mean(coefficients))
 
-	plt.figure(1, figsize=(9.5, 7))
+	# plt.figure(1, figsize=(9.5, 7))
 
-	plt.subplot(2,1,1)
-	plt.plot(np.arange(x.size)/float(fs), x, 'b')
-	plt.axis([0, x.size/float(fs), min(x), max(x)])
-	plt.ylabel('amplitude')
-	plt.title('x (speech-male.wav)')
+	# plt.subplot(2,1,1)
+	# plt.plot(np.arange(x.size)/float(fs), x, 'b')
+	# plt.axis([0, x.size/float(fs), min(x), max(x)])
+	# plt.ylabel('amplitude')
+	# plt.title('x (speech-male.wav)')
 
-	plt.subplot(2,1,2)
-	numFrames = int(mfccs[:,0].size)
-	frmTime = H*np.arange(numFrames)/float(fs)
-	plt.pcolormesh(frmTime, 1+np.arange(12), np.transpose(mfccs[:,1:]))
-	plt.ylabel('coefficients')
-	plt.title('MFCCs')
-	plt.autoscale(tight=True)
-	plt.tight_layout()
-	plt.savefig('mfcc.png')
-	plt.show()
+	# plt.subplot(2,1,2)
+	# numFrames = int(mfccs[:,0].size)
+	# frmTime = H*np.arange(numFrames)/float(fs)
+	# plt.pcolormesh(frmTime, 1+np.arange(12), np.transpose(mfccs[:,1:]))
+	# plt.ylabel('coefficients')
+	# plt.title('MFCCs')
+	# plt.autoscale(tight=True)
+	# plt.tight_layout()
+	# plt.savefig('mfcc.png')
+	# plt.show()
 
 	return headers, features
 
-print get_features('../processed_data/ed/ED003')
+# print get_features('../processed_data/ed/ED003')
 # print get_features('../../sms-tools/workspace/Tabla_test/ED003_PS_LLL_1.wav')
--- run_features.py
+++ run_features.py
@@ -20,57 +20,40 @@ metadata_cols = []
 
 # Generate audio features
 
-def write_features(file, patient_dir, create_header):
-    headers = []
-    features = []
+def write_features(file, patient_id, patient_dir, create_header):
+    headers = ['id']
+    features = [patient_id]
 
-    stft_headers, stft_features = stft.get_features(patient_dir)
-    headers = headers + stft_headers
-    features = features + stft_features
+    # stft_headers, stft_features = stft.get_features(patient_dir)
+    # headers = headers + stft_headers
+    # features = features + stft_features
 
-    mfcc_headers, mfcc_features = mfcc_features.get_features(patient_dir)
+    mfcc_headers, mfcc_features = mfcc.get_features(patient_dir)
     headers = headers + mfcc_headers
     features = features + mfcc_features
 
     if create_header:
         file.write(','.join(headers) + '\n')
-    file.write(','.join(features) + '\n')
+    file.write(','.join(map(str, features)) + '\n')
 
-        # Iterate through all patient data
-        for patient_id in patient_ids:
-            try:
-                features = get_features(patient_id)
-                print features
-                output_file.write('%s,' % patient_id)
-                for feature in features[:-1]:
-                    output_file.write('%f,' % feature)
-                output_file.write('%f' % features[-1])
-                output_file.write('\n')
-            except:
-                # do nothing
-                print 'something'
 
 def get_patient_ids(path, study):
-    patient_ids = [file for file in os.listdir(audio_file_path) if file.startswith(study)]
+    patient_ids = [file for file in os.listdir(path) if file.startswith(study)]
     patient_ids = [patient_id for patient_id in patient_ids if patient_id not in bad_patient_ids]
-    return patient_ids.sort()
-    return ["%s/%s" % (path, patient_id) for patient_id in patient_ids]
+    patient_ids.sort()
+    return patient_ids
 
-patient_dirs = \
-    + \
-    get_patient_dirs('%s/ed' % in_root, 'ED') + \
 
 file = open(audio_features_path, "w")
 create_header=True
+print get_patient_ids('%s/healthy' % in_root, 'HA')
 for patient_id in get_patient_ids('%s/healthy' % in_root, 'HA'):
-    if create_header:
-        file.write('id,')
-    write_features(file, '%s/healthy/%s' % (in_root, patient_id), create_header)
+    write_features(file, patient_id, '%s/healthy/%s' % (in_root, patient_id), create_header)
     if create_header:
         create_header=False
 
 for patient_id in get_patient_ids('%s/ed' % in_root, 'ED'):
-    write_features(file, '%s/ed/%s' % (in_root, patient_id), create_header)
+    write_features(file, patient_id, '%s/ed/%s' % (in_root, patient_id), create_header)
 
 # Process metadata
 sys.exit(0)
