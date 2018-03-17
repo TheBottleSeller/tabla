88716b4ea860642353d6885e0ffb20ce928df327
szetumer <szetumer@gmail.com>
2018-03-17 01:00:02 -0700

    Update run_pipe.py

--- run_pipe.py
+++ run_pipe.py
@@ -4,9 +4,8 @@ import re
 sys.path.append('./analysis')
 import pipe_to_model as pipe
 
-
+#this file takes raw data, but you could have it take any type of data you want
 in_root = 'raw_data'
-
 
 def walk_and_process_audio(path,all_files = []):
     in_dir = os.path.join(in_root, path)
@@ -24,9 +23,10 @@ def walk_and_process_audio(path,all_files = []):
                 next
     return all_files
 
-
+#get a list of all the audio files you want to look at.
 all_files = walk_and_process_audio('')
 
+#for this analysis, we're just going to extracct sound type.
 def sound_type(file_name):
     PS = re.search('PS', file_name)
     if PS:
@@ -41,6 +41,11 @@ def sound_type(file_name):
 
 sound_type = [sound_type(x) for x in all_files]
 
+#This says:
+#split the frequency into 257 bins, and the waveform into 60000 pieces.
+#and take the first 201 sound files, and categorize them by sound type.
+#and get dataframes of the frequency and the waveform, and perform a pca
+#on the frequency spectra.
 data = pipe.get_frames_and_models(257,
                                   60000,
                                   all_files[0:200],
@@ -49,4 +54,3 @@ data = pipe.get_frames_and_models(257,
                                   perc_training = .8,
                                   directory = "./analysis/"
                                   )
-
